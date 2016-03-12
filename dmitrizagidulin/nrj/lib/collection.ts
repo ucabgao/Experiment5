874b528e67fbf51effb837bf39a0a8b4fb8acc66
simon.gaozheng <simon.gaozheng@gmail.com>
2016-03-12 21:44:56 +0000

    annotation

--- dmitrizagidulin/nrj/lib/collection.ts
+++ dmitrizagidulin/nrj/lib/collection.ts
@@ -15,55 +15,79 @@ specific language governing permissions and limitations
 under the License.
 **/
 //var CollectionSchema = require('./collection_schema');
-var CollectionSchema;
+var CollectionSchema:any;
+
 var Collection = function Collection(name, client) {
-    this.name = name;
-    this.client = client;
-};
+  this.name = name;
+  this.client = client;
+}
+
 Collection.prototype.delete_raw_json = function delete_raw_json(key, callback) {
-    this.client.delete_raw_json(this.name, key, callback);
-};
+  this.client.delete_raw_json(this.name, key, callback);
+}
+
 Collection.prototype.find_by_key = function find_by_key(key, callback) {
-    this.get_raw_json(key, callback);
-};
+  this.get_raw_json(key, callback);
+}
+
 Collection.prototype.find_all = function find_all(query, callback) {
-    this.client.get_query_all(this.name, query, callback);
-};
+  this.client.get_query_all(this.name, query, callback);
+}
+
 Collection.prototype.find_one = function find_one(query, callback) {
-    this.client.get_query_one(this.name, query, callback);
-};
+  this.client.get_query_one(this.name, query, callback);
+}
+
 Collection.prototype.get_raw_json = function get_raw_json(key, callback) {
-    this.client.get_json_object(this.name, key, callback);
-};
+  this.client.get_json_object(this.name, key, callback);
+}
+
+interface t {
+key:any;
+to_json_document:s;
+}
+
+interface s {
+(x:{}): string;
+}
+
 Collection.prototype.get_schema = function get_schema(callback) {
-    this.client.get_schema_json(this.name, callback);
-};
-Collection.prototype.insert = function insert(doc, callback) {
-    this.insert_raw_json(doc.key, doc.to_json_document, callback);
-};
-Collection.prototype.insert_raw_json = function insert_raw_json(key, json_obj, callback) {
-    this.client.insert_raw_json(this.name, key, json_obj, callback);
-};
+  this.client.get_schema_json(this.name, callback);
+}
+
+Collection.prototype.insert = function insert(doc:t, callback) {
+  this.insert_raw_json(doc.key, doc.to_json_document, callback);
+}
+
+Collection.prototype.insert_raw_json = function insert_raw_json(key, json_obj:string, callback) {
+  this.client.insert_raw_json(this.name, key, json_obj, callback);
+}
+
 Collection.prototype.new_schema = function new_schema() {
-    return new CollectionSchema(this.name);
-};
+  return new CollectionSchema(this.name);
+}
+
 Collection.prototype.remove = function remove(doc, callback) {
-    this.delete_raw_json(doc.key, callback);
-};
+  this.delete_raw_json(doc.key, callback);
+}
+
 Collection.prototype.update_raw_json = function update_raw_json(key, json_obj, callback) {
-    this.client.update_raw_json(this.name, key, json_obj, callback);
-};
+  this.client.update_raw_json(this.name, key, json_obj, callback);
+}
+
 Collection.prototype.set_schema = function set_schema(schema, callback) {
-    var schema_fields;
-    if (!schema) {
-        throw "Invalid schema object";
-    }
-    if (schema instanceof CollectionSchema) {
-        schema_fields = schema.build();
-    }
-    else {
-        schema_fields = schema;
-    }
-    this.client.set_schema(this.name, schema_fields, callback);
-};
+  var schema_fields;
+  
+  if(!schema) {
+    throw "Invalid schema object";
+  }
+  if(schema instanceof CollectionSchema) {
+    schema_fields = schema.build();
+  } else {
+    schema_fields = schema;
+  }
+  
+  this.client.set_schema(this.name, schema_fields, callback);
+}
+
 //module.exports.Collection = Collection;
