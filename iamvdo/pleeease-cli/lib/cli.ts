b6d8e283a0ee8ee9e04d9f4c3984b650fbb38ee5
Earl Barr <E.Barr@ucl.ac.uk>
2016-08-20 12:59:34 +0100

    annotation

--- iamvdo/pleeease-cli/lib/cli.ts
+++ iamvdo/pleeease-cli/lib/cli.ts
@@ -4,14 +4,19 @@
 /// <reference path="deep-extend.d.ts" />
 /// <reference path="chokidar.d.ts" />
 /// <reference path="node.d.ts" />
+
 'use strict';
-var Pleeease = require('pleeease'), Logger = require('../lib/logger');
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var extend = require('deep-extend');
-var chokidar = require('chokidar');
-var multiglob = require('multi-glob');
+
+var Pleeease     = require('pleeease'),
+    Logger       = require('../lib/logger');
+
+var fs           = require('fs');
+var path         = require('path');
+var mkdirp       = require('mkdirp');
+var extend       = require('deep-extend');
+var chokidar     = require('chokidar');
+var multiglob    = require('multi-glob');
+
 /**
  *
  * Walk a path and return all files
@@ -19,51 +24,59 @@ var multiglob = require('multi-glob');
  * @return {Array}  results Array of files
  *
  */
-var walk = function (dir) {
+var walk = function(dir) {
     var results = [];
     var list = fs.readdirSync(dir);
-    list.forEach(function (file) {
+    list.forEach(function(file) {
         file = dir + '/' + file;
         var stat = fs.statSync(file);
         if (stat && stat.isDirectory()) {
             results = results.concat(walk(file));
-        }
-        else {
+        } else {
             results.push(file);
         }
     });
     return results;
 };
+
 /**
  *
  * Constructor CLI
  *
  */
 var CLI = function () {
+
     this.defaults = {
-        'in': '*.css',
+        'in' : '*.css',
         'out': 'app.min.css'
     };
     this.configFile = '.pleeeaserc';
+
     var opts = this.extendConfig();
+
     this.pleeease = new Pleeease(opts);
+
 };
+
 /**
  *
  * Extend config with .pleeeaserc
  *
  */
 CLI.prototype.extendConfig = function (opts) {
-    opts = opts || {};
-    // read pleeeaserc
-    var config = {};
-    try {
-        config = JSON.parse(fs.readFileSync(this.configFile, 'utf-8'));
-    }
-    finally {
-        return extend(opts, config);
-    }
+
+  opts = opts || {};
+
+  // read pleeeaserc
+  var config = {};
+  try {
+    config = JSON.parse(fs.readFileSync(this.configFile, 'utf-8'));
+  } finally {
+    return extend(opts, config);
+  }
+
 };
+
 /**
  *
  * Initialize CLI
@@ -74,43 +87,60 @@ CLI.prototype.extendConfig = function (opts) {
  *
  */
 CLI.prototype.init = function (fn, inputs, output) {
+
     var cli = this;
-    inputs = this.pleeease.options.in || this.pleeease.options.sourcemaps.from || inputs;
-    output = this.pleeease.options.out || this.pleeease.options.sourcemaps.to || output;
+
+    inputs = this.pleeease.options.in  || this.pleeease.options.sourcemaps.from || inputs;
+    output = this.pleeease.options.out || this.pleeease.options.sourcemaps.to   || output;
+
     multiglob.glob(inputs, function (err, files) {
+
         if (err) {
             throw err;
         }
+
         if (!files.length) {
             return new Logger.error('File(s) not found');
         }
+
         var results = [];
+
         // for each file / dir in arguments
         for (var i = 0; i < files.length; i++) {
+
             // if it's a directory
             if (fs.statSync(files[i]).isDirectory()) {
                 // walk path and get all files
                 results = results.concat(walk(files[i]));
-            }
-            else {
+            } else {
                 // it's a file
                 results.push(files[i]);
             }
+
         }
+
         // remove output from inputs
         if (results.indexOf(output) !== -1) {
             results.splice(results.indexOf(output), 1);
         }
+
         if ('compile' === fn) {
+
             cli.compile(results, output);
-        }
-        else if ('watch' === fn) {
+
+        } else if ('watch' === fn) {
+
             cli.watch(results, output);
+
         }
+
         // log
         return new Logger.success('Compile ' + results.length + ' file(s) [' + results + '] to ' + output);
+
     });
+
 };
+
 /**
  *
  * Compile all inputs files and write output file
@@ -119,42 +149,52 @@ CLI.prototype.init = function (fn, inputs, output) {
  *
  */
 CLI.prototype.compile = function (inputs, output) {
+
     var cli = this;
+
     // create a new (future) Root AST
     var root;
+
     // get inputs files
-    inputs.map(function (filename) {
+    inputs.map(function(filename) {
+
         // read, parse and concatenate rules to Root AST
         var filestring = fs.readFileSync(filename, 'utf-8');
         if (cli.pleeease.options.sourcemaps) {
             cli.pleeease.options.sourcemaps.from = filename;
         }
         var fileAst = cli.pleeease.parse(filestring);
+
         // create the final AST
         if (root === undefined) {
             root = fileAst;
-        }
-        else {
-            fileAst.each(function (rule) {
+        } else {
+            fileAst.each(function(rule) {
                 root.append(rule.clone());
             });
         }
+
     });
+
     var fixed = cli.pleeease.process(root);
+
     // create directory if not exists
     mkdirp(path.dirname(output), function (err) {
+
         if (err) {
             console.log(err);
         }
         if (cli.pleeease.options.sourcemaps && cli.pleeease.options.sourcemaps.map.inline === false) {
             fs.writeFileSync(output, fixed.css);
             fs.writeFileSync(output + '.map', fixed.map);
-        }
-        else {
+        } else {
             fs.writeFileSync(output, fixed);
         }
+
     });
+
 };
+
 /**
  *
  * Watch all files, and write processed file when a change is detected
@@ -163,13 +203,17 @@ CLI.prototype.compile = function (inputs, output) {
  *
  */
 CLI.prototype.watch = function (inputs, output) {
+
     var cli = this;
+
     // compile first
     cli.compile(inputs, output);
+
     // ignore all files that don't end with .css + output file
     var ignore = function (path) {
         return (path.match(/\.css$/) && path === output);
     };
+
     /**
      *
      * Watcher
@@ -179,25 +223,32 @@ CLI.prototype.watch = function (inputs, output) {
      * - `usePolling` (needed for some text editors)
      *
      */
-    var watcher = chokidar.watch(['.'], { ignored: ignore, persistent: true, usePolling: true });
-    watcher
-        .on('change', function (path) {
-        // when a change is detected, compile files
-        cli.compile(inputs, output);
-        // log
-        return new Logger.success('Recompiled file ' + path);
-    });
+    var watcher = chokidar.watch(['.'], {ignored: ignore, persistent: true, usePolling: true});
+        watcher
+        .on('change', function(path) {
+
+            // when a change is detected, compile files
+            cli.compile(inputs, output);
+
+            // log
+            return new Logger.success('Recompiled file ' + path);
+
+        });
+
     // log
     return new Logger.log('Watcher is running...');
+
 };
+
 /**
  *
  * New CLI instance
  *
  */
-var cli = function () {
+var cli = function() {
     return new CLI();
 };
+
 /**
  *
  * Exports
