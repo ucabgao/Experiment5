f6941a4e9b0c1edecf0b4e40684dcc88f12715ec
Earl Barr <E.Barr@ucl.ac.uk>
2016-08-25 10:57:33 +0100

    annotation

---
+++ ukiuni/Slite/routes/api/image.ts
@@ -0,0 +1,161 @@
+var express = require('express');
+var router = express.Router();
+var AccessKey = global.db.AccessKey;
+var Content = global.db.Content;
+var ContentBody = global.db.ContentBody;
+var AccountInGroup = global.db.AccountInGroup;
+var Promise = require("bluebird");
+var Random = require(__dirname + "/../../util/random");
+var Storage = require(__dirname + "/../../util/storage");
+var ERROR_NOTACCESSIBLE = "ERROR_NOTACCESSIBLE";
+router.get('/:imageKey', function(req, res) {
+	if (!req.params.imageKey || req.params.imageKey.indexOf("/../") > -1) {
+		res.status(404).send();
+		return;
+	}
+	Storage.load(req.params.imageKey).then(function(file) {
+		if (file.redirectUrl) {
+			res.set("Pragma", "no-cache");
+			res.redirect(302, file.redirectUrl);
+		} else {
+			res.set('Content-Type', file.contentType);
+			res.status(200).send(file.buffer);
+		}
+	})["catch"](function(error) {
+		console.log(error.stack);
+		res.status(404).send();
+	});
+})
+function getImage(req, res, name) {
+	if (!req.params.contentKey || !req.params.imageKey || req.params.imageKey.indexOf("/../") > -1) {
+		res.status(404).send();
+		return;
+	}
+	var loadedContent;
+	Content.find({
+		where : {
+			accessKey : req.params.contentKey
+		}
+	}).then(function(content) {
+		if (!content) {
+			throw ERROR_NOTACCESSIBLE;
+		}
+		loadedContent = content;
+		return ContentBody.find({
+			where : {
+				ContentId : content.id,
+				version : content.currentVersion
+			}
+		});
+	}).then(function(contentBody) {
+		if (contentBody.status == ContentBody.STATUS_OPEN || contentBody.status == ContentBody.STATUS_URLACCESS) {
+			return new Promise(function(success) {
+				success()
+			});
+		} else {
+			var accessKey = req.query.sessionKey || req.query.access_token || req.cookies["session_key"];
+			if (!accessKey) {
+				throw ERROR_NOTACCESSIBLE;
+			}
+			accessKey = accessKey.replace("\"", "").replace("\"", "");// for
+			// cookie;
+			return AccessKey.findBySessionKey(accessKey).then(function(accessKey) {
+				if (!accessKey) {
+					throw ERROR_NOTACCESSIBLE;
+				}
+				if (accessKey.AccountId == loadedContent.ownerId) {
+					return new Promise(function(success) {
+						success("authorized")
+					});
+				} else if (ContentBody.STATUS_AUTHENTICATEDONLY != content.ContentBodies[0].status) {
+					throw ERROR_NOTACCESSIBLE;
+				}
+				return AccountInGroup.find({
+					where : {
+						GroupId : content.GroupId,
+						AccountId : accessKey.AccountId,
+						inviting : Group.INVITING_DONE
+					}
+				});
+			}).then(function(accountInGroup) {
+				if (!accountInGroup) {
+					throw ERROR_NOTACCESSIBLE;
+				}
+			});
+		}
+	}).then(function() {
+		Storage.load(req.params.contentKey + "/" + req.params.imageKey, name).then(function(file) {
+			if (name && file.name == name) {
+				if (file.redirectUrl) {
+					res.set("Pragma", "no-cache");
+					res.redirect(302, file.redirectUrl);
+				} else {
+					res.set('Content-Type', "application/octet-stream");
+					res.set('Content-Disposition', 'attachment; filename="' + name + '"');
+					res.status(200).send(file.buffer);
+				}
+			} else if (!name) {
+				if (file.redirectUrl) {
+					res.set("Pragma", "no-cache");
+					res.redirect(302, file.redirectUrl);
+				} else {
+					res.set('Content-Type', file.contentType);
+					res.status(200).send(file.buffer);
+				}
+			} else {
+				res.status(404).end();
+			}
+		})
+	})["catch"](function(error) {
+		if (ERROR_NOTACCESSIBLE == error) {
+			res.status(403).send();
+		} else {
+			console.log(error.stack);
+			res.status(500).send();
+		}
+	});
+}
+router.get('/:contentKey/:imageKey', function(req, res) {
+	getImage(req, res);
+});
+router.get('/:contentKey/:imageKey/:fileName', function(req, res) {
+	getImage(req, res, req.params.fileName);
+});
+router.post('/:contentKey', function(req, res) {
+	var accessKey = req.body.sessionKey || req.body.access_token;
+	if (!accessKey) {
+		res.status(403).end();
+		return;
+	}
+	AccessKey.findBySessionKey(accessKey).then(function(accessKey) {
+		if (!accessKey) {
+			throw ERROR_NOTACCESSIBLE;
+		}
+		return accessKey.getAccount();
+	}).then(function(account) {
+		if (!account) {
+			throw ERROR_NOTACCESSIBLE;
+		}
+		accessAccount = account;
+		return Random.createRandomBase62();
+	}).then(function(random) {
+		imageFileKey = random;
+		return Storage.store(req.params.contentKey + "/" + imageFileKey, req.files.imageFile[0].mimetype, req.body.name, req.files.imageFile[0], accessAccount.id);
+	}).then(function(savedImageUrl) {
+		res.status(200).send({
+			url : savedImageUrl
+		});
+	})["catch"](function(error) {
+		if (ERROR_NOTACCESSIBLE == error) {
+			res.status(403).send();
+		} else {
+			if (error.stack) {
+				console.log(error.stack);
+			} else {
+				console.log(error);
+			}
+			res.status(500).send();
+		}
+	});
+});
+module.exports = router;
